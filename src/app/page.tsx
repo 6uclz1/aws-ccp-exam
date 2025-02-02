3f6f03884f5a7a112ae45a6f6ebdb321a52bdda5
ぶち <6uclz1@users.noreply.github.com>
2025-02-02 23:06:58 +0900

    fix style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import QuizComponent from "./quizComponent";
 
 export default function Home() {
   return (
-    <div className="py-8">
+    <div className="py-8 px-2">
       <QuizComponent/>
     </div>
   );
